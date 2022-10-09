14697659a3e9734bad39fc1714b84805cca3bbc6
manikanta <manikanta1407@gmail.com>
2022-10-09 15:59:55 +0530

    adding sum function

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
-
+int sum(int a,int b)
+{
+    return a+b;
+}
 
 int main()
-{
+{       int n;
 	cout << "Hello World";
+       cout << sum(20,30)<<endl;
 	return 0;
+
 }
