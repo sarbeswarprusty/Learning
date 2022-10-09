c7996bfd23c12f4638e7ed66a11ba1b0567a833c
manikanta <manikanta1407@gmail.com>
2022-10-09 23:28:18 +0530

    adding addition functionly

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,8 +6,14 @@ int modulation_function(int a,int b)
 	return a%b;
 }
 
+int add(int a,int b)
+{ 
+return a+b;
+}
+
 int main()
 {
 	cout << "Hello World"<<modulation_function(50,10)<<endl;
+	cout << "Hello World"<<add(10,20)<<endl;
 	return 0;
 }
