666265e86ac88a181dd40da2b9feeb500455462a
manikanta <manikanta1407@gmail.com>
2022-10-09 23:16:43 +0530

    adding modulation function

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int modulation_function(int a,int b)
+{
+	return a%b;
+}
 
 int main()
 {
-	cout << "Hello World";
+	cout << "Hello World"<<modulation_function(50,10)<<endl;
 	return 0;
 }
